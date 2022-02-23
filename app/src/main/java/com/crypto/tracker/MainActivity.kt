65860ddf550263df55d37605f082f06d399dd285
Elnur <elnur@Elnurs-MacBook-Pro-2.local>
2022-02-23 18:26:50 +0400

    navigation component, pagination was added

--- app/src/main/java/com/crypto/tracker/MainActivity.kt
+++ app/src/main/java/com/crypto/tracker/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.crypto.tracker
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.crypto.tracker.databinding.ActivityMainBinding
+import com.crypto.tracker.utils.setupWithNavigationController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
+
+    private var destination = 0
 
     private val mainViewModel: MainViewModel by viewModels()
     private val binding: ActivityMainBinding by lazy {
@@ -18,5 +30,85 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        setupBottomNavigationBar()
+    }
+
+    private fun setupBottomNavigationBar() {
+        setSupportActionBar(binding.toolbar)
+        val navGraphIds = listOf(
+            R.navigation.navigation_home,
+            R.navigation.navigation_history,
+            R.navigation.navigation_favorite)
+
+        binding.navView.setOnItemSelectedListener {
+
+            when (it.itemId) {
+                R.id.navigation_home -> {
+                }
+                R.id.navigation_history -> {
+                }
+                R.id.navigation_favorite -> {
+                }
+            }
+            Log.e("Data", it.title.toString())
+
+            true
+        }
+
+        val controller = binding.navView.setupWithNavigationController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.nav_host_container,
+            intent = intent
+        )
+
+        controller.observe(this) { navController ->
+            navController.removeOnDestinationChangedListener(addOnDestinationChangedListener)
+            navController.addOnDestinationChangedListener(addOnDestinationChangedListener)
+            setupActionBarWithNavController(navController)
+        }
+
+        var navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment
+
+        var navController = navHostFragment.navController
+        controller.postValue(navController)
+    }
+
+    @SuppressLint("RestrictedApi")
+    private val addOnDestinationChangedListener =
+        NavController.OnDestinationChangedListener { _, destination, _ ->
+
+            if (this@MainActivity.destination != destination.id) {
+                this@MainActivity.destination = destination.id
+
+                when (destination.id) {
+                    R.id.homeFragment -> {
+                        supportActionBar?.setDisplayHomeAsUpEnabled(false)
+                    }
+                    else -> {
+                        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+                    }
+                }
+
+                when (destination.id) {
+
+                    R.id.homeFragment,
+                    R.id.historyFragment,
+                    R.id.favoriteFragment
+                    -> {
+                        binding.navView.visibility = View.VISIBLE
+                    }
+                    else -> {
+                        binding.navView.visibility = View.GONE
+                    }
+                }
+            }
+        }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
--- app/src/main/java/com/crypto/tracker/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/crypto/tracker/presentation/home/HomeViewModel.kt
@@ -1,9 +1,23 @@
 package com.crypto.tracker.presentation.home
 
 import androidx.lifecycle.ViewModel
+import com.crypto.tracker.model.remote.response.CryptoModel
+
 
 class HomeViewModel: ViewModel() {
     val adapter = HomeAdapter{
         //to do on click
+    }
+
+    init {
+        setDummyTestCrypto()
+    }
+
+    private fun setDummyTestCrypto() {
+        val list = arrayListOf<CryptoModel>()
+        list.add(CryptoModel("1","test1"))
+        list.add(CryptoModel("2","test2"))
+
+        // set it to paginable adapter
     }
 }
